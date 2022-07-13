9735432c64906d41bbc642394a691959d3d06944
awaisshah228 <m.awaisshah228.com>
2022-07-13 12:51:32 +0500

    updatd

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,21 @@
+require('dotenv').config();
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
+
+// Replace this private key with your Goerli account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Beware: NEVER put real Ether into testing accounts
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [PRIVATE_KEY]
+    }
+  }
 };
